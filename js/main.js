718262e556df96a2ee46878bc36003a614a350af
James Croft <james@gomake.com.au>
2015-03-22 15:47:20 +1000

    add adblock check

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $('#calculate').click(function () {
         calculateTimeCode(timecode);
     } else {
         $('.panel-answer').removeClass("panel-default hide").addClass("panel-danger show");
-        $('#answer').html("Total: Sorry, please input valid timecodes in hours, minutes and seconds (eg. 0:22, 3:26, 15:26, 4:23:59), and only one kind of seperator.");
+        $('#answer').html("Sorry, please input valid timecodes in hours, minutes and seconds (eg. 0:22, 3:26, 15:26, 4:23:59), and only one kind of seperator.");
         $('#timecodes-answer').html("");
     };
 
@@ -145,3 +145,67 @@ $('#calculate').click(function () {
             });
     }
 });
+
+(function(window, undefined) {
+
+function Adblocked() {
+  this.scriptFile = "//pagead2.googlesyndication.com/pagead/show_ads.js";
+}
+
+Adblocked.prototype.isAdblocked = function() {
+  if (typeof(window.google_ad_block) === "undefined") {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+Adblocked.prototype.done = function(ctx) {
+  ctx = (typeof(ctx) === "undefined") ? this : ctx 
+  if (ctx.isAdblocked())  {
+    window.adblocked.result = true
+  }
+  else {
+    window.adblocked.result = false
+  }
+  var error = null;
+  window.adblocked.userCallback(error, window.adblocked.result)
+}
+
+Adblocked.prototype.insert = function() {
+  var body = document.getElementsByTagName('body')[0]
+  var ad = document.createElement("div")
+  ad.style.display = "none";
+  adScript = document.createElement("script")
+  adScript.setAttribute("type","text/javascript")
+  adScript.setAttribute("src",this.scriptFile)
+  body.appendChild(ad).appendChild(adScript)
+  that = this
+  adScript.onload = function() {that.done(that)};
+  adScript.onerror = adScript.onload;
+  return this
+}
+
+var checkAds = function(userCallback) {
+  if (typeof(userCallback) !== "undefined") { 
+    window.adblocked.userCallback = userCallback
+  }
+  var a = new Adblocked()
+  // if it appears that ads are blocked already
+  if (a.isAdblocked()) {
+    a.insert() // attempt to load ads
+  }
+  // if ads have already loaded
+  else {
+    a.done()    
+  }
+}
+
+window.adblocked = {
+  check : checkAds,
+  userCallback : function() {},
+  result : "unknown"
+}
+
+})(window)
